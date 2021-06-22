8d16dbd0165162e28c67f738d902ac2abdc0511c
drypot <drypot@gmail.com>
2021-06-22 14:35:48 +0900

    update.

---
+++ src/hello-world/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl">Hello React!!</h1>
+
+    </div>
+  );
+}
+
+
--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './tailwind.css'
-import './base.css'
 import App from './App'
 
 ReactDOM.render(
